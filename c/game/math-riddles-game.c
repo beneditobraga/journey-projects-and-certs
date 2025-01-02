6646fd9d04eafbdd63b224f60b4870ed5503dd42
Benedito Braga (Benê) <152809017+beneditobraga@users.noreply.github.com>
2025-01-01 23:13:27 -0300

    Building Initial Structure

--- c/game/math-riddles-game.c
+++ c/game/math-riddles-game.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+// Game Data Structure
+typedef struct 
+{
+   //  
+   int difficulty;
+   int value1;
+   int value2;
+   int operation;
+   int result;
+
+} calculate;
+
+// Functions
+void play();
+void showInfo(calculate calc);
 
 int main ()
 {
-
+    jogar();
 
     return 0;
+}
+
+// Implementations
+void play()
+{
+
+}
+
+void showInfo(calculate calc)
+{
+    
 }
