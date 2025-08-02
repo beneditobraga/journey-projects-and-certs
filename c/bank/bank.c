2fd36ef428813fbf981e4622dab6d3931f0e9357
Benedito Braga (Benê) <152809017+beneditobraga@users.noreply.github.com>
2025-08-02 12:13:25 -0300

    start Project

--- c/bank/bank.c
+++ c/bank/bank.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h> // Only linux or MacOS | sleep()
+// #include <windows.h> For Windows | Sleep()
+#include <time.h>
 
 int main ()
 {
