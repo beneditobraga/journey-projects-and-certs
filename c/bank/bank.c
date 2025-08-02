d4c73e6a73dedde218bb712460af5e9d37532f57
Benedito Braga (Benê) <152809017+beneditobraga@users.noreply.github.com>
2025-08-02 12:17:35 -0300

    Create Struct Client

--- c/bank/bank.c
+++ c/bank/bank.c
@@ -5,6 +5,17 @@
 // #include <windows.h> For Windows | Sleep()
 #include <time.h>
 
+typedef struct 
+{
+    int code;
+    char name[50];
+    char email[50];
+    char cpf[20];
+    char dateBirth[20];
+    char dateRegistration[20];
+};
+
+
 int main ()
 {
 
