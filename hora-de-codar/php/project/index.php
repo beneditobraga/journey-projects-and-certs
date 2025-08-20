758b39d6fad0db8e3fb8451c7540234be80ebba0
Benedito Braga (Benê) <152809017+beneditobraga@users.noreply.github.com>
2025-08-20 14:48:32 -0300

    Update CSS in Project PHP by Hora de Codar

--- hora-de-codar/php/project/index.php
+++ hora-de-codar/php/project/index.php
@@ -1,7 +1,22 @@
 <?php
     include_once("templates/header.php");
 ?>
-    <h1>Test</h1>
+    <main>
+        <div id="title-container">
+            <h1>Tutor Blog</h1>
+            <p>Tutoriais de Tecnologia</p>
+        </div>
+        <div id="posts-container">
+            <?php foreach($posts as $post): ?>
+                <div class="post-box">
+                    <img src="images/<?= $post['img']?>" alt="">
+                    <h2 clas="post-title">
+                        <a href=""><?= $post['title'] ?></a>
+                    </h2>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </main>
 <?php
     include_once("templates/footer.php");
 ?>
